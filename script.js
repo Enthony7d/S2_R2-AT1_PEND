79caaba033660ce0180ee4dfe6b3e2a2eedf96fe
ENTHONY OLIVEIRA DE SOUZA <enthony.souza2@senaisp.edu.br>
2026-03-12 09:58:00 -0300

    exercicio 3

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function mudarTexto() {
-    document.getElementById("texto").innerText = "Texto alterado com JavaScript!";
-}
+// Seleciona o botão de adicionar
+const addBtn = document.getElementById("addBtn");
+
+// Adiciona evento de clique
+addBtn.addEventListener("click", function() {
+    const input = document.getElementById("itemInput");
+    const texto = input.value.trim();
+
+    if(texto !== ""){
+        const li = document.createElement("li");
+        li.textContent = texto;
+
+        document.getElementById("lista").appendChild(li);
+
+        input.value = ""; // limpa o input
+        input.focus();    // mantém o foco
+    }
+});
